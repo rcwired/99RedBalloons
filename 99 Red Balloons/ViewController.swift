6b6f413c47696e51bbd366d2b1b8909c01bf33ac
Richard Hubbard <rchubbard@Ricks-MacBook-Pro.local>
2015-05-27 19:11:50 -0500

    first commit

--- 99 Red Balloons/ViewController.swift
+++ 99 Red Balloons/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var imageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextButtonPressed(sender: UIBarButtonItem) {
+        
+        println("Hello World")
+        
+    }
 
 }
 
